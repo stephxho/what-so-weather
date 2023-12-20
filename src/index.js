f6357b9fc20c92b1aebb76e28b24abbfefc53030
stephxho <149762989+stephxho@users.noreply.github.com>
2023-12-20 10:35:05 +0100

    Updated details and description content in the html/css

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
+function searchCity(city) {
+  //make api call and update the interface
+  let apiKey = "8b568tdd52ea0467a4ff3f2cbo8f1f31";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  console.log(apiUrl);
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-input");
   let currentCity = document.querySelector("#current-city");
   currentCity.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
